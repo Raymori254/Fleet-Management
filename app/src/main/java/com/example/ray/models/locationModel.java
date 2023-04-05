424845d7ef9398d38877de05b650766fa05ba9e0
Rayhans123 <raymori254@gmail.com>
2023-04-05 18:20:16 +0300

    Get the userlocation and add it to the database

---
+++ app/src/main/java/com/example/ray/models/locationModel.java
@@ -0,0 +1,33 @@
+package com.example.ray.models;
+
+public class locationModel {
+
+
+    private double longitude;
+    private double latitude;
+
+    public locationModel() {
+
+    }
+
+    public locationModel(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
